e44303a90e9cbfa84cd774a60096b7de1111c5b9
kotilevskyim <kotilevskyim@gmail.com>
2020-07-07 12:01:56 +0300

    Play Tracks with AVPlayerKit

--- MyMusic/Search/TrackDetailView/TrackDatailView.swift
+++ MyMusic/Search/TrackDetailView/TrackDatailView.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
-
+import SDWebImage
+import AVKit
 
 class TrackDetailView: UIView {
     
     @IBOutlet weak var trackImageView: UIImageView!
-    
     @IBOutlet weak var currentTimeSlider: UISlider!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var currentTimeLabel: UILabel!
@@ -20,10 +20,39 @@ class TrackDetailView: UIView {
     @IBOutlet weak var authorTitleLabel: UILabel!
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var volumeSlider: UISlider!
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+//        минимизирует задержку воспроизведения
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         trackImageView.backgroundColor = .red
     }
+    
+    func set(viewModel: SearchViewModel.Cell) {
+        trackTitleLabel.text = viewModel.trackName
+        authorTitleLabel.text = viewModel.artistName
+        playTrack(previewUrl: viewModel.previewUrl)
+        let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
+        guard let url = URL(string: string600 ?? "") else {return}
+        trackImageView.sd_setImage(with: url, completed: nil)
+        
+    }
+    
+    private func playTrack(previewUrl: String?) {
+        print("PLAY TRACK by link: \(previewUrl ?? "NIL")")
+        
+        guard let url = URL(string: previewUrl ?? "") else {return}
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    
+    }
+    
     @IBAction func dragDownButtonTapped(_ sender: Any) {
 //        drag down
         self.removeFromSuperview()
@@ -37,6 +66,14 @@ class TrackDetailView: UIView {
     @IBAction func nextTrack(_ sender: Any) {
     }
     @IBAction func playPauseAction(_ sender: Any) {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "Pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
+        
     }
     
 }
